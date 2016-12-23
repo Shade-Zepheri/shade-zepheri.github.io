44e82ac29fc8ebc10c7d0db9c9f748cfd22a82aa
Shade-Zepheri <ziroalpha@gmail.com>
2016-12-23 16:27:34 -0600

    Fixes

--- depic/src/script.js
+++ depic/src/script.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
+(function($){
+  $(function(){
     $('.collapsible').collapsible();
   });
+})(jQuery);
+
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
