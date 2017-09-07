2accd0b4fbc6150e3bf3cba366001f93171c5849
Shade-Zepheri <ziroalpha@gmail.com>
2017-09-07 13:32:24 -0500

    [Depic] Fixed version check?

--- depic/src/ios_version_check.js
+++ depic/src/ios_version_check.js
@@ -6,71 +6,19 @@ let me know and I'll credit you.
 
 const VERSION_CHECK_SUPPORTED = "Your iOS version is supported! &#x1f60a;";
 const VERSION_CHECK_NEEDS_UPGRADE = "Requires at least iOS %s &#x1f615;";
-const VERSION_CHECK_UNCONFIRMED = "Not yet tested on iOS %s &#x1f601;";
-const VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s &#x1f61e;";
+const VERSION_CHECK_UNCONFIRMED = "Not yet tested on iOS %s &#x2190;";
 
-function ios_version_check(minIOS, maxIOS, otherIOS, callBack) {
-    "use strict";
+function checkIOSVersion(minIOS, maxIOS) {
+    var iOS = parseFloat(
+        ('' + (/CPU.*OS ([0-9_]{1,5})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0, ''])[1])
+        .replace('undefined', '3_2').replace('_', '.').replace('_', '')
+    ) || false;
 
-
-    function parseVersionString(version) {
-        var bits = version.split(".");
-        return [bits[0], bits[1] ? bits[1] : 0, bits[2] ? bits[2] : 0];
+    if (iOS > maxIOS) {
+        return VERSION_CHECK_UNCONFIRMED.replace("%s", iOS);
+    } else if (iOS < minIOS) {
+        return VERSION_CHECK_NEEDS_UPGRADE.replace("%s", minIOS);
+    } else if (iOS >= minIOS && maxIOS >= iOS) {
+        return VERSION_CHECK_SUPPORTED;
     }
-
-    function compareVersions(one, two) {
-        // https://gist.github.com/TheDistantSea/8021359
-        for (var i = 0; i < one.length; ++i) {
-            if (two.length == i) {
-                return 1;
-            }
-
-            if (one[i] == two[i]) {
-                continue;
-            } else if (one[i] > two[i]) {
-                return 1;
-            } else {
-                return -1;
-            }
-        }
-
-        if (one.length != two.length) {
-            return -1;
-        }
-
-        return 0;
-    }
-
-    var version = navigator.appVersion.match(/CPU( iPhone)? OS (\d+)_(\d+)(_(\d+))? like/i);
-    if (!version) {
-        return 0;
-    }
-
-    var osVersion = [version[2], version[3], version[4] ? version[5] : 0],
-
-        osString = osVersion[0] + "." + osVersion[1] + (osVersion[2] && osVersion[2] != 0 ? "." + osVersion[2] : ""),
-        minString = minIOS,
-        maxString = maxIOS,
-
-        minVersion = parseVersionString(minString),
-        maxVersion = maxString ? parseVersionString(maxString) : null,
-
-        message = VERSION_CHECK_SUPPORTED,
-        isBad = false;
-
-    if (compareVersions(minVersion, osVersion) == 1) {
-        message = VERSION_CHECK_NEEDS_UPGRADE.replace("%s", minString);
-        isBad = true;
-    } else if (maxVersion && compareVersions(maxVersion, osVersion) == -1) {
-        if ("unsupported" == otherIOS) {
-            message = VERSION_CHECK_UNSUPPORTED.replace("%s", minString).replace("%s", maxString);
-        } else {
-            message = VERSION_CHECK_UNCONFIRMED.replace("%s", osString);
-        }
-
-        isBad = true;
-    }
-    callBack(message, isBad);
-
-    return (isBad ? -1 : 1);
 }
