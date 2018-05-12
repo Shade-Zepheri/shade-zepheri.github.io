357093441064513363866c1b1b2340a1568333f6
Shade-Zepheri <ziroalpha@gmail.com>
2018-05-11 22:18:36 -0500

    [Home] Simplified .js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
     M.AutoInit();
 });
 
-function getParameterByName(name, url) {
-    if (!url) {
-        url = window.location.href;
+function getParameterByName(name) {
+    let params = new URLSearchParams(location.search.slice(1));
+
+    if (params.has("p")) {
+        return params.get("p");
     }
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+
+    return "";
 }
