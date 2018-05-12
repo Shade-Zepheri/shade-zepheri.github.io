4a5a9bdf0c1aa7f02d1a1c9cd17ea23a851fea1c
Shade-Zepheri <ziroalpha@gmail.com>
2018-05-12 13:43:26 -0500

    [Depic] Hopefilly fixed not working in Cydia

--- depic/src/script.js
+++ depic/src/script.js
@@ -1,1 +1,18 @@
-function getParameterByName(e){let a=new URLSearchParams(location.search.slice(1));return a.has(e)?a.get(e):""}jQuery,M.AutoInit();
+(function($) {
+    $(function() {
+        M.AutoInit();
+    });
+})(jQuery);
+
+function getUrlParams(prop) {
+    var params = {};
+    var search = decodeURIComponent(window.location.href.slice(window.location.href.indexOf('?') + 1));
+    var definitions = search.split('&');
+
+    definitions.forEach(function(val, key) {
+        var parts = val.split('=', 2);
+        params[parts[0]] = parts[1];
+    });
+
+    return (prop && prop in params) ? params[prop] : params;
+}
