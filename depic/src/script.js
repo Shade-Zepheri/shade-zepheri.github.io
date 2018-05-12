06345731e4bf5236a03fc9609e4f808d4ad90d10
Shade-Zepheri <ziroalpha@gmail.com>
2018-05-11 22:33:44 -0500

    [Depic] Updated depics

--- depic/src/script.js
+++ depic/src/script.js
@@ -1,17 +1,13 @@
-(function($){
-  $(function(){
-    $('.collapsible').collapsible();
-  });
+(function($) {
+    M.AutoInit();
 })(jQuery);
 
-function getParameterByName(name, url) {
-    if (!url) {
-      url = window.location.href;
+function getParameterByName(name) {
+    let params = new URLSearchParams(location.search.slice(1));
+
+    if (params.has("p")) {
+        return params.get("p");
     }
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+
+    return "";
 }
